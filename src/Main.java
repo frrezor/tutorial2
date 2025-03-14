248a85376490f787f3028d23beef4ed00cfaccdf
Daniil <s33932@pjwstk.edu.pl>
2025-03-14 17:03:59 +0100

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+         int [] array1= {1,2,3,4,5,6};
+          int[] OddArray1=  filterIndexOddValue(array1);
+        String result = Arrays.toString(OddArray1);
+        System.out.println( Arrays.toString(OddArray1));
+
+
+    }
+
+    public static int[] filterIndexOddValue(int [] arr) {
+           int count =0;
+        for ( int i = 0; i < arr.length; i++ ) {
+             if(arr[i]%2!=0) {
+                 count++;
+             }
+        }
+        int [] OddArray= new int[count];
+        int index=0;
+        for ( int i = 0; i < arr.length; i++ ) {
+
+            if(arr[i]%2!=0) {
+                OddArray[index++]=arr[i];
+            }
+
+        }
+         return OddArray;
+    }
+}
