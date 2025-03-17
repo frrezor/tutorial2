b808a544f3118f4721e85b84c5976fe532be3660
Daniil <s33932@pjwstk.edu.pl>
2025-03-17 20:16:52 +0100

    first task

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
          int [] array1= {1,2,3,4,5,6};
           int[] OddArray1=  filterIndexOddValue(array1);
-        String result = Arrays.toString(OddArray1);
-        System.out.println( Arrays.toString(OddArray1));
+          System.out.println(Arrays.toString(OddArray1));
+
 
 
     }
 
     public static int[] filterIndexOddValue(int [] arr) {
-           int count =0;
-        for ( int i = 0; i < arr.length; i++ ) {
-             if(arr[i]%2!=0) {
+         int count =0;
+         for ( int i = 0 ; i < arr.length ; i++) {
+             if ( arr[i] % 2 != 0 ) {
                  count++;
              }
-        }
-        int [] OddArray= new int[count];
-        int index=0;
-        for ( int i = 0; i < arr.length; i++ ) {
-
-            if(arr[i]%2!=0) {
-                OddArray[index++]=arr[i];
-            }
-
-        }
+         }
+         int[] OddArray= new int[count];
+         int index=0;
+         for ( int i = 0 ; i < arr.length ; i++) {
+             if ( arr[i] % 2 != 0 ) {
+                 OddArray[index++]=arr[i];
+             }
+         }
          return OddArray;
     }
 }
